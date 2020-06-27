83553321c22da2e41cd73fb6b758f7543e2fb276
amankkg <amankubanychbek@gmail.com>
2020-06-27 20:55:23 +0600

    finish hw: add sum of two numbers

--- Program.cs
+++ Program.cs
@@ -6,8 +6,14 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello GitHub!");
+            var a = Console.ReadLine();
+            var b = Console.ReadLine();
+
+            var sum = Add(int.Parse(a), int.Parse(b));
+
+            Console.WriteLine(sum);
         }
+
+        static int Add(int a, int b) => a + b;
     }
 }
